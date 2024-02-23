4269e047e562835a5df3b596c02ea88c85648eb2
Lagrangejess <wni@naver.com>
2024-02-23 10:22:15 +0900

    2.0

--- consumer/Consumer_temp.py
+++ consumer/Consumer_temp.py
@@ -12,12 +12,17 @@ redis_client = redis.StrictRedis(host='54.180.141.165', port=31724, db=0)
 for message in consumer:
     # JSON 디코딩
     received_message = json.loads(message.value.decode('utf-8'))
-    
-    #Dict to string
-    received_message = json.dumps(received_message)
 
+    #key,value 분류
+    for key, value in received_message.items():
+        redis_key = key
+        redis_value = json.dumps(value) 
+   
     # Redis에 메시지 저장
-    redis_client.set('agv', received_message)
-    
+    print(redis_key) 
+    print(redis_value)    
+    redis_client.set(redis_key, redis_value)
+
+
     # 저장된 메시지 출력
-    print(f"Received: {received_message}")
+    #print(f"Received: {received_message}")
